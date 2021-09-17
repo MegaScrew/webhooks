793b0298a6aed0e875cc9e45e1483726c07199bd
MegaScrew <MegaScrew@mail.ru>
2021-09-17 14:08:37 +0300

    added function dateISO

--- function.inc.php
+++ function.inc.php
@@ -1,7 +1,20 @@
 <?php 
 require_once 'crest.php';
 
+/**
+*  Returns the date in ISO8601 format
+* @var $day integer, specifies how many days to add to the current date
+* @return date format ISO8601
+*/
 
+function dateISO(int $day = 0){
+	if ($day == 0) {
+		$date = time();
+	}else{
+		$date = strtotime('+'.$day.' day');
+	}
+	return date(DateTime::ISO8601, $date);
+}
 
 
 ?>
